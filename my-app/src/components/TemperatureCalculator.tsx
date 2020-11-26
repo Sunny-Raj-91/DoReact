199c9147585c6fa6a57ad59938bdd8037feb295c
Sunny Raj <sunny.raj202@gmail.com>
2020-11-27 02:52:15 +0530

    d

--- my-app/src/components/TemperatureCalculator.tsx
+++ my-app/src/components/TemperatureCalculator.tsx
@@ -2,10 +2,11 @@ import React, { Component } from 'react';
 import TemperatureInput from './TemperatureInput';
 
 class TemperatureCalculator extends Component<{},{ctemp : string,ftemp : string,celcius : string,farenheit:string}> {
+    boil: string
     
     constructor(props:{}){
         super(props)
-        
+        this.boil = ""
         this.state = {
             ctemp : "",
             ftemp : "",
@@ -13,12 +14,14 @@ class TemperatureCalculator extends Component<{},{ctemp : string,ftemp : string,
             farenheit : "farenheit"
         }
         this.handleTempChange = this.handleTempChange.bind(this)
+        
     }
     handleTempChange(e: { target: { value: any; }; },scale : string){
         console.log(this.state)
         if(scale === this.state.celcius){
             
         this.setState(p => {
+            
               return { ...p,ctemp:e.target.value,ftemp : (((parseFloat(e.target.value)*9 + 160)/5).toString())}
         })
         
@@ -28,14 +31,15 @@ class TemperatureCalculator extends Component<{},{ctemp : string,ftemp : string,
             return {...p,ctemp : (((parseFloat(e.target.value)*5 - 160)/9).toString()),ftemp:e.target.value}
         })
     }
-    componentDidUpdate(){
-        console.log(this.state)
-    }
+
+
     render() {
         return (
             <div>
                 <TemperatureInput scale={this.state.celcius} temp={this.state.ctemp} handleTempChange = {this.handleTempChange} />
                 <TemperatureInput scale = {this.state.farenheit} temp={this.state.ftemp} handleTempChange = {this.handleTempChange} />
+                <h5>{this.boil = (parseFloat(this.state.ctemp) >= 100) ?  "Water will boil" : "water will not boil"  } </h5>
+               
             </div>
         );
     }
